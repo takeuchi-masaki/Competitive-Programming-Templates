4862f2c32d7e187f70999ce8ed826c5ded57a53f
takeuchi-masaki <themasakitakeuchi@gmail.com>
2024-06-09 16:07:23 -0700

    update xoshiro256

--- xoshiro256.cpp
+++ xoshiro256.cpp
@@ -1,6 +1,7 @@
-#include <bits/stdc++.h>
+#pragma once
 #include <chrono>
-using namespace std;
+#include <cstdint>
+#include <cmath>
 
 constexpr static uint64_t splitmix64(uint64_t x) { // https://doi.org/10.1145/2714064.2660195
     x += 0x9e3779b97f4a7c15;
@@ -13,18 +14,21 @@ constexpr static uint64_t splitmix64(uint64_t x) { // https://doi.org/10.1145/27
     xoshiro256++ https://prng.di.unimi.it/xoshiro256plusplus.c
     States initialized using splitmix as suggested
 */
-struct xoshiro {
+struct xoshiro256 {
     uint64_t state[4]; // 256 bits state
     static inline uint64_t rotl(const uint64_t x, int k) {
         return (x << k) | (x >> (64 - k));
     }
-    xoshiro() {
-        uint64_t x = chrono::steady_clock::now().time_since_epoch().count();
+    xoshiro256() {
+        uint64_t x = std::chrono::steady_clock::now().time_since_epoch().count();
         for (int i = 0; i < 4; i++) {
             x = splitmix64(x);
             state[i] = x;
         }
     }
+    /*
+        generates a random number between [0, u64 max]
+    */
     uint64_t random64() {
         const uint64_t result = rotl(state[0] + state[3], 23) + state[0];
         const uint64_t t = state[1] << 17;
@@ -37,20 +41,10 @@ struct xoshiro {
         return result;
     }
     /*
-     * https://xoroshiro.di.unimi.it/random_real.c
-     * random_real_64: Pick an integer in {0, 1, ..., 2^64 - 1} uniformly
-     * at random, convert it to double, and divide it by 2^64.  Values in
-     * [2^-11, 1] are overrepresented, small exponents have low precision,
-     * and exponents below -64 are not possible.
-     */
-    double random_real_64() {
-        return ldexp((double)random64(), -64);
-    }
-    /*
         get unbiased random number in range [0, high) from uniformly random 2^n
         Fast Random Integer Generation in an Interval (Lemire, 2018) https://arxiv.org/abs/1805.10941
     */
-    uint64_t next0_H(uint64_t high) {
+    uint64_t nextInt(uint64_t high) {
         uint64_t x = random64();
         __uint128_t m = (__uint128_t)x * (__uint128_t)high;
         uint64_t l = (uint64_t)m;
@@ -68,9 +62,9 @@ struct xoshiro {
         uniform int distribution
         get a random number in the range [low, high]
     */
-    uint64_t uid(uint64_t low, uint64_t high) {
+    uint64_t nextInt(uint64_t low, uint64_t high) {
         uint64_t range = high - low + 1;
-        return next0_H(range) + low;
+        return nextInt(range) + low;
     }
 };
 
